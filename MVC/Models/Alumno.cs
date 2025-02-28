8b5bf7b9737ad172afe44bb27782ebc52fda4fb9
Lisseth <arguetalisseth029@gmail.com>
2025-02-28 01:15:48 -0600

    Creando Migraciones - Tablas Alumnos

---
+++ MVC/Models/Alumno.cs
@@ -0,0 +1,46 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Diagnostics.CodeAnalysis;
+
+namespace MVC.Models
+{
+    public class Alumno
+    {
+        #region Propiedades
+        [Key]
+        public int AlumnoId { get; set; }
+        [NotNull]
+        public string Nombres { get; set; }
+        [NotNull]
+        public int Edad { get; set; }
+        [NotNull]
+        public string Carnet { get; set; }
+        public int CantidadMaterias { get; set; }
+        public int Ciclo { get; set; }
+        public string Apellidos { get; set; }
+        public DateTime FechaNacimiento { get; set; }
+        [NotMapped]
+        public string NombreCompleto { get; set; }
+        #endregion Propiedades
+
+        #region Constructor
+        public Alumno(string nombres, int edad, string apellidos, DateTime fechaNacimiento)
+        {
+            NombreCompleto = nombres;
+            Edad = edad;
+            Apellidos = apellidos;
+            FechaNacimiento = fechaNacimiento;
+            NombreCompleto = $"{Nombres} {Apellidos}";
+        }
+        #endregion Constructor
+
+        #region Funciones
+
+
+        public bool EsMayordeEdad()
+        {
+            return Edad > 18;
+        }
+        #endregion Funciones
+    }
+}
